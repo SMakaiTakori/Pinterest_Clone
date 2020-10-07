e72948af7a9453f398227785afe250339727fb34
smakaitakori <makait87@icloud.com>
2020-10-07 01:24:43 -0400

    added img tags and displaying images on DOM

--- pinterest_clone_client/src/components/PinsContainer.js
+++ pinterest_clone_client/src/components/PinsContainer.js
@@ -8,41 +8,43 @@ const PinsContainer = () => {
     const [query, setQuery] = useState('');  
     const [imageData, setImageData] = useState('flowers');
     const [pinsData, setPinsData] = useState([]);
-    console.log('pinsData',pinsData)
     
-    useEffect(() => {
-        
+    
+    useEffect(() => {        
         fetch(`https://pixabay.com/api/?key=18540307-a22579c944a9b0e0dd7d2004b&q=${query}`)
         .then(res => res.json())
         .then(data => 
             setPinsData(data.hits));
     }, [query]);
 
-    const HitsData = () => {          
-            return pinsData.map(hit => (
-                console.log('HitsData', hit)
-            ))
-    }
+    const HitsData = () => {               
+            return pinsData.map(hit => (                                                   
+                <>
+                <br/>   
+                <br/>
+                <a href= '#'><img alt='' src={hit.previewURL} /></a>              
+                   <span>
+                   <br/>                 
+                        Likes: {hit.favorites} 
+                        <br/>    
+                        Owner: { hit.user}
+                    </span>
+                </>   
+            ))}
 
-    return(
-        
+                
+    return(             
         <>
         <div>
            <h1>Find your next inspiration!</h1>
            <input 
-            type='text' 
-            onChange= {(e) => setImageData(e.target.value)}
-            value={imageData}
-            placeholder='Search'
-           />
-           <button onClick={() => setQuery(imageData)} >Get Images</button>
-           <p> Total images related to {query}: {pinsData.total} </p>
-           <p> Total Hits:{pinsData.totalHits} </p>  
-
-
-            {pinsData.length ? < HitsData /> : null}
-            {console.log('inside JSX',pinsData)}
-            
+                type='text' 
+                onChange= {(e) => setImageData(e.target.value)}
+                value={imageData}
+                placeholder='Search'
+           /> 
+           <button onClick={() => setQuery(imageData)}> Get Images </button>           
+           {pinsData.length ? < HitsData /> : null}                    
         </div>
         </>
     );
