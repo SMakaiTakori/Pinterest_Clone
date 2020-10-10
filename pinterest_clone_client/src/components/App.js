68e3a4a03455ca3c9a5783da5eeb18ddf649816f
smakaitakori <makait87@icloud.com>
2020-10-10 14:22:52 -0400

    added navBar component, added styling for navBar moved logo

--- pinterest_clone_client/src/components/App.js
+++ pinterest_clone_client/src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import logo from '../images/pinterest-icon.png'
 import PinsContainer from './PinsContainer'
 import CategoriesContainer from './CategoriesContainer'
+import NavBar from './navBar'
 
 
 
@@ -13,10 +14,13 @@ const App = () => {
 
     return(
         <div>
-            <img alt={logo} src={ logo } style={{ float:'left', width:'60px', height:'60px' }} />
+            {/* <img alt={logo} src={ logo } style={{ float:'left', width:'60px', height:'60px' }} />
             <h1 style= {{ color: '#E60023', paddingTop:'25px'}}>
                 Clone
-            </h1>
+               
+            </h1> */}
+            
+            <NavBar />
             <br/>
             <h1 >Find your Pinspiration!</h1>
             <PinsContainer favorite={favorite} setFavorite={setFavorite} selected={selected} />
--- pinterest_clone_client/src/components/navBar.js
+++ pinterest_clone_client/src/components/navBar.js
@@ -1,1 +1,24 @@
-import React from 'react'
+import React from 'react';
+import logo from '../images/pinterest-icon.png';
+
+
+const NavBar = () => {
+
+    return(
+        <div >
+            
+            <ul id="nav">
+            <img alt={logo} src={ logo } style={{ float:'left', width:'60px', height:'60px',  paddingTop:'10px' }} />
+            <h1 style= {{ color: '#E60023'}}> 
+                Clone
+                <li><a href='#'> About </a></li>
+                <li><a href='#'> My Pins </a></li>
+                <li><a href='#'> Home </a></li>          
+                
+            </h1>
+            </ul>
+        </div>
+    )
+}
+
+export default NavBar;
